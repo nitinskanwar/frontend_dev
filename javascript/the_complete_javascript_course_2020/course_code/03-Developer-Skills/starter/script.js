07290edbf4188bbd8ac42eb345ea9b30191c161f
Nitin Kanwar <nitinkanwar@Nitins-MacBook-Pro.local>
2021-04-06 16:57:57 -0500

    Added projects folder

--- javascript/the_complete_javascript_course_2020/course_code/03-Developer-Skills/starter/script.js
+++ javascript/the_complete_javascript_course_2020/course_code/03-Developer-Skills/starter/script.js
@@ -1,4 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-console.log('Hello World1!');
+const printForecast = function(arr) {
+    const length_of_array = arr.length;
+    let string_to_print = '';
+    for (let i = 1; i <= length_of_array; i++) {
+        string_to_print = string_to_print + arr[i - 1] + 'C in ' + i + ' days.. ';
+    }
+    return string_to_print;
+};
+
+console.log(printForecast([17, 21, 23]));
